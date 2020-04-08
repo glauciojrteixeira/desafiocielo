9742605dccfa938e431c0455008e68b72688a5f7
Glaucio J Teixeira <glaucio.teixeira@engdb.com.br>
2020-04-08 12:45:14 -0300

    Primeiro commit

---
+++ src/main/java/br/com/elo/desafiocielo/DesafiocieloApplication.java
@@ -0,0 +1,13 @@
+package br.com.elo.desafiocielo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocieloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocieloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/elo/desafiocielo/DesafiocieloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elo.desafiocielo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocieloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
