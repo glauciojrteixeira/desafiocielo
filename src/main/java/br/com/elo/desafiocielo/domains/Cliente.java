f408b202ce1bf2aff38d78f641b752647e00de48
Glaucio J Teixeira <glaucio.teixeira@engdb.com.br>
2020-04-13 21:04:38 -0300

    Refactor Cliente.

--- src/main/java/br/com/elo/desafiocielo/domains/Cliente.java
+++ src/main/java/br/com/elo/desafiocielo/domains/Cliente.java
@@ -18,6 +18,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.elo.desafiocielo.domains.enums.TipoCliente;
 import br.com.elo.desafiocielo.domains.enums.TipoPerfil;
+import lombok.EqualsAndHashCode;
 
 /**
  * 
@@ -28,6 +29,7 @@ import br.com.elo.desafiocielo.domains.enums.TipoPerfil;
  */
 
 @Entity
+@EqualsAndHashCode(of={"id"})
 public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -53,8 +55,9 @@ public class Cliente implements Serializable {
 	private String senha;
 	
 	
-	/* Telefone representado por um conjunto de string não repetivel
-	 * Entidade fraca dispensa existencia de classe
+	/*
+	 * Telefone representado por um conjunto de string não repetivel
+	 * Entidade fraca dispensa existencia de classe.
 	 */
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
@@ -150,33 +153,4 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 	
-	/**
-	 * Hash and Equals
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
-
 }
